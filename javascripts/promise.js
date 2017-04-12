b25c6be8a691412556b90a2db19dc6cb4982e646
Mitchell Blom <mitchellblom@gmail.com>
2017-04-12 12:30:43 -0500

    class notes updated

--- javascripts/promise.js
+++ javascripts/promise.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
     var dinosaurs = [];
 
 
-    function writeDOM() {
+    function writeDOM(){
         var domString = "";
-        for (var i = 0; i < dinosaurs.length; i++) {
+        for(var i=0; i<dinosaurs.length; i++){
             domString += `<h1>${dinosaurs[i].type}</h1>`;
         }
         $("#promises").append(domString);
     }
 
-
+    //ajax pryamid of doom
     // $.ajax("./db/dinosaurs1.json").done(function(data1){
     //     console.log("data1", data1.dinosaurs1);
     //     dinosaurs = data1.dinosaurs1
@@ -37,47 +37,92 @@ $(document).ready(function() {
     // })
 
 
-    var firstDinosaurJSON = function() { // This goes in a IIFE, like a getter
-        return new Promise(function(resolve, reject) {
-            $.ajax("./db/dinosaurs1.json").done(function(data1) {
+    var firstDinosaurJSON = function(){
+        return new Promise(function(resolve, reject){
+            $.ajax("./db/dinosaurs1.json").done(function(data1){
                 resolve(data1.dinosaurs1);
-            }).fail(function(error) {
+            }).fail(function(error1){
                 reject(error1);
             })
         })
-    }
+    };
 
-    var secondDinosaurJSON = function() { // This goes in a IIFE, like a getter
-        return new Promise(function(resolve, reject) {
-            $.ajax("./db/dinosaurs2.json").done(function(data2) {
+    var secondDinosaurJSON = function(){
+        return new Promise(function(resolve, reject){
+            $.ajax("./db/dinosaurs2.json").done(function(data2){
                 resolve(data2.dinosaurs2);
-            }).fail(function(error) {
+            }).fail(function(error2){
                 reject(error2);
             })
         })
-    }
+    };
 
-    var thirdDinosaurJSON = function() { // This goes in a IIFE, like a getter
-        return new Promise(function(resolve, reject) {
-            $.ajax("./db/dinosaurs3.json").done(function(data3) {
+    var thirdDinosaurJSON = function(){
+        return new Promise(function(resolve, reject){
+            $.ajax("./db/dinosaurs3.json").done(function(data3){
                 resolve(data3.dinosaurs3);
-            }).fail(function(error) {
+            }).fail(function(error3){
                 reject(error3);
             })
         })
-    }
-
-    firstDinosaurJSON().then(function(jsonData1) { // .then grabs resolve		// This goes in a DOMhandler, like a setter
-        console.log(jsonData1);
-        dinosaurs = jsonData1;
-        writeDOM();
-    }).catch(function(jsonDataFail1) {
-        console.log(jsonDataFail1); // .catch grabs reject
-    });
+    };
 
 
-    // More class notes go here...
+    // pyramid of doom with promises
+    // firstDinosaurJSON().then(function(jsonData1){
+    //     console.log(jsonData1);
+    //     dinosaurs = jsonData1;
+    //     secondDinosaurJSON().then(function(jsonData2){
+    //         console.log(jsonData2);
+    //         jsonData2.forEach(function(dino){
+    //             dinosaurs.push(dino);
+    //         })
+    //         thirdDinosaurJSON().then(function(jsonData3){
+    //         console.log(jsonData3);
+    //         jsonData3.forEach(function(dino){
+    //             dinosaurs.push(dino);
+    //         })
+    //         writeDOM();
+    //     }).catch(function(jsonDataFail3){
+    //         console.log(jsonDataFail3);
+    //     });
+    //     }).catch(function(jsonDataFail2){
+    //         console.log(jsonDataFail2);
+    //     });
+    // }).catch(function(jsonDataFail1){
+    //     console.log(jsonDataFail1);
+    // });
 
 
+    // this is the most ideal method
+    // firstDinosaurJSON().then(function(jsonData1){
+    //     console.log(jsonData1);
+    //     dinosaurs = jsonData1;
+    //     return secondDinosaurJSON();
+    // }).then(function(jsonData2){
+    //     console.log(jsonData2);
+    //     jsonData2.forEach(function(dino){
+    //         dinosaurs.push(dino);
+    //     })
+    //     return thirdDinosaurJSON();
+    // }).then(function(jsonData3){
+    //     console.log("jsonData2", jsonData2);
+    //     console.log(jsonData3);
+    //     jsonData3.forEach(function(dino){
+    //         dinosaurs.push(dino);
+    //     })
+    //     writeDOM();
+    // });
 
+    //this method works the best for this solution
+    Promise.all([firstDinosaurJSON(), secondDinosaurJSON(), thirdDinosaurJSON()])
+        .then(function(resultz){
+            console.log("resultz", resultz);
+            resultz.forEach(function(ajaxCalls){
+                ajaxCalls.forEach(function(dino){
+                    dinosaurs.push(dino);
+                })
+            })
+            writeDOM();
+        })
 });
